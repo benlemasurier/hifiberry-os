3022501a54b344153f782c790e5b340ca80735da
Ben LeMasurier <ben.lemasurier@gmail.com>
2021-11-30 11:10:58 -0700

    webradio: drop TuneIn, use radio-browser.net

--- buildroot/package/webradio/webradio.mk
+++ buildroot/package/webradio/webradio.mk
@@ -4,11 +4,8 @@
 #
 ################################################################################
 
-WEBRADIO_VERSION = 83be60bcc4404cccf38e27b0747cfd50fd4a693e
-WEBRADIO_SITE = $(call github,juangaimaro,radio,$(WEBRADIO_VERSION))
-
-#WEBRADIO_VERSION = 38109377aec2bcd6f85671cc986c3a4ce45f5d80
-#WEBRADIO_SITE = $(call github,hifiberry,radio,$(WEBRADIO_VERSION))
+WEBRADIO_VERSION = 5ee8d746b3bab7f6847ce161f9b21364c10d3d3f
+WEBRADIO_SITE = $(call github,benlemasurier,radio,$(WEBRADIO_VERSION))
 
 define WEBRADIO_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0555 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/webradio/start-radio \
